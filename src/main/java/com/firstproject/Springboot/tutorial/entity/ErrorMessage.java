599598c1ff00d4be5c0547c0106ccf6a8298d11f
tanmaypardeshi <tanmaypardeshi@gmail.com>
2021-08-13 12:18:49 +0530

    Removed test case for now

--- src/main/java/com/firstproject/Springboot/tutorial/entity/ErrorMessage.java
+++ src/main/java/com/firstproject/Springboot/tutorial/entity/ErrorMessage.java
@@ -1,45 +1,19 @@
 package com.firstproject.Springboot.tutorial.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ErrorMessage {
     @Id
     private HttpStatus status;
     private String message;
-
-    public ErrorMessage() {
-    }
-
-    public ErrorMessage(HttpStatus status, String message) {
-        this.status = status;
-        this.message = message;
-    }
-
-    public HttpStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(HttpStatus status) {
-        this.status = status;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    @Override
-    public String toString() {
-        return "ErrorMessage{" +
-                "status=" + status +
-                ", message='" + message + '\'' +
-                '}';
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
 }
